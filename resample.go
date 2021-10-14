5e3824315263be483c10f92cfa19069dc1dbc3a0
joypauls <joypaulsen3@gmail.com>
2021-10-14 18:17:03 -0500

    working on simplest minimal api

--- resample.go
+++ resample.go
@@ -1,6 +1,7 @@
 package resample
 
 import (
+	"errors"
 	"math/rand"
 	"time"
 )
@@ -8,18 +9,36 @@ import (
 // Core interface that any sampling method should implement
 type Sampler interface {
 	// Shuffle() []float64
-	Sample(n int, replacement bool) []float64
+	Sample(n int, replacement bool) ([]float64, error)
 }
 
+// is there actually any benefit for a sampler to have data? for resampling probably, for a single sample? meh
 type SimpleSampler struct {
 	data []float64
 }
 
-func (s *SimpleSampler) Sample(n int, replacement bool) []float64 {
-	rand.Seed(time.Now().UnixNano())
-	sample := make([]float64, n)
-	for i := range sample {
-		sample[i] = s.data[rand.Intn(len(s.data))]
+func (s *SimpleSampler) Sample(k int, replacement bool) ([]float64, error) {
+	// disallow oversampling for simple use case
+	if k > len(s.data) {
+		return []float64{}, errors.New("Invalid sample size requested")
 	}
-	return sample
+	rand.Seed(time.Now().UnixNano()) // should this always be done?
+	sample := make([]float64, k)
+	if replacement {
+		// with replacement
+		for i := 0; i < k; i++ {
+			sample[i] = s.data[rand.Intn(len(s.data))]
+		}
+		return sample, nil
+	}
+	// without replacement
+	idx := rand.Perm(len(s.data))
+	for i := 0; i < k; i++ {
+		sample[i] = s.data[idx[i]]
+	}
+	return sample, nil
+}
+
+func MakeSimpleSampler(data []float64) SimpleSampler {
+	return SimpleSampler{data: data}
 }
--- resample_test.go
+++ resample_test.go
@@ -3,10 +3,23 @@ package resample
 import "testing"
 
 func TestSimpleSampler(t *testing.T) {
-	s := SimpleSampler{data: []float64{12.2, 54.1, 77.7}}
+	s := MakeSimpleSampler([]float64{12.2, 54.1, 77.7})
 
-	// test size
-	if res, exp := len(s.Sample(1, false)), 1; res != exp {
+	// test size, w/o replacement
+	sample, err := s.Sample(2, false)
+	if res, exp := len(sample), 2; res != exp && err != nil {
 		t.Errorf("Result: %d, Wanted: %d", res, exp)
+	}
+
+	// test size, w/ replacement
+	sample, err = s.Sample(2, true)
+	if res, exp := len(sample), 2; res != exp && err != nil {
+		t.Errorf("Result: %d, Wanted: %d", res, exp)
+	}
+
+	// test prevention of oversampling
+	sample, err = s.Sample(4, true)
+	if err == nil {
+		t.Errorf("Result: %e, Wanted: nil", err)
 	}
 }
